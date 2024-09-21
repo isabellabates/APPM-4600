c4affcda4042828e04d466161b64185c89b2395a
Isabella Bates <isba8475@colorado.edu>
2024-09-20 19:13:35 -0600

    update

---
+++ Homework/Homework 3/q3.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def driver():
+    a = 1
+    b = 4
+    tol = 1e-3
+    # part a
+    print("3 (b)")
+    f = lambda x: x**3 + x - 4
+    # call bisection routine
+    [astar,ier,count] = bisection(f,a,b,tol)
+    print('The approximate root is:', astar)
+    print('The error message reads:', ier)
+    print('f(astar) =', f(astar))
+    print('Total number of iterations:', count) # print the # of iterations
+
+# define routines
+def bisection(f,a,b,tol):
+    # Inputs:
+    # f,a,b - function and endpoints of initial interval
+    # tol - bisection stops when interval length < tol
+    # Returns:
+    # astar - approximation of root
+    # ier - error message
+    # ier = 1 => Failed
+    # ier = 0 == success
+
+    # check there is a root in the interval
+    fa = f(a)
+    fb = f(b)
+    if (fa*fb>0):
+       ier = 1
+       astar = a
+       return [astar, ier, 0]
+
+    # verify end points are not a root 
+    if (fa == 0):
+      astar = a
+      ier =0
+      return [astar, ier, 0]
+
+    if (fb ==0):
+      astar = b
+      ier = 0
+      return [astar, ier, 0]
+
+    count = 0
+    d = 0.5*(a+b)
+    while (abs(d-a)> tol):
+        fd = f(d)
+        if (fd ==0):   
+           astar = d
+           ier = 0
+           return [astar, ier,count]
+        if (fa*fd<0):
+           b = d
+        else: 
+           a = d
+           fa = fd
+        d = 0.5*(a+b)
+        count = count +1
+    
+    astar = d
+    ier = 0
+    return [astar, ier, count]
+      
+driver()   
---
+++ Homework/Homework 3/q5.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def driver():
+    # 5(a) plot f(x)= x-4sin(2x)-3
+    f = lambda x: x - 4*np.sin(2*x) - 3
+    # x = np.linspace(-10,10,1000)
+    x = np.linspace(-3,10,1000)
+    y = f(x)
+
+    plt.plot(x,y,color='orange')
+    plt.hlines(0, -3, 10, colors='red', linestyles='dashed')
+    plt.xlabel('x')
+    plt.xticks(np.arange(-3, 10, 1))
+    plt.ylabel('y')
+    plt.title('f(x)=x-4sin(2x)-3')
+    plt.show()
+driver()
